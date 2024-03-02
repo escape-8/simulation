c5dbf09b178448d5ecf76f95ecebad91890933dc
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-03-02 17:13:49 +0300

    refactor: remove count render symbols from multiple to one

--- src/Grass.php
+++ src/Grass.php
@@ -3,11 +3,8 @@
 namespace Simulation;
 class Grass extends Entity
 {
-    private const GRASS =  ["\u{1F353}", "\u{1F330}", "\u{1F33D}"];
     public function __toString(): string
     {
-        $count = count(self::GRASS) - 1;
-        $symbol = random_int(0, $count);
-        return self::GRASS[$symbol];
+        return "\u{1f9c0}";
     }
 }
--- src/Tree.php
+++ src/Tree.php
@@ -4,11 +4,8 @@ namespace Simulation;
 
 class Tree extends Entity
 {
-    private const TREES = ["\u{1F333}", "\u{1F332}"];
-
     public function __toString()
     {
-        $symbol = random_int(0, count(self::TREES) - 1);
-        return self::TREES[$symbol];
+        return "\u{1F333}";
     }
 }
