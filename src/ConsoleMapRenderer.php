29dcb3b85fe650fbc3809525d1e639c0e7e440ad
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-03-02 12:37:13 +0300

    refactor: change render empty parts of map

--- src/ConsoleMapRenderer.php
+++ src/ConsoleMapRenderer.php
@@ -5,6 +5,9 @@ namespace Simulation;
 class ConsoleMapRenderer
 {
     private const EMPTY_SYMBOL = "\u{0020}";
+    private const BYTES_IN_UNICODE_SYMBOL = 2;
+    private const ANSI_BG_COLOR = "\u{001B}[0;100m";
+    private const ANSI_RESET_COLOR = "\u{001B}[0m";
     public function render(Map $map): void
     {
         for ($positionY = 0; $positionY < Map::$height; $positionY++) {
@@ -13,7 +16,8 @@ class ConsoleMapRenderer
                 if ($map->haveEntityOnPosition($positionX, $positionY)) {
                     $line[] = $map->getEntity($positionX, $positionY);
                 } else {
-                    $line[] = self::EMPTY_SYMBOL;
+                    $emptyPart = str_repeat(self::EMPTY_SYMBOL, self::BYTES_IN_UNICODE_SYMBOL);
+                    $line[] = self::ANSI_BG_COLOR .  $emptyPart . self::ANSI_RESET_COLOR;
                 }
             }
             $strLine = implode(' ', $line) . "\n";
