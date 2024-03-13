81bbde66f6732ec03dc19a0243f1ec9403281ea1
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-03-13 21:55:17 +0300

    refactor: change render parameters in render loop

--- src/ConsoleMapRenderer.php
+++ src/ConsoleMapRenderer.php
@@ -10,11 +10,12 @@ class ConsoleMapRenderer
     private const ANSI_RESET_COLOR = "\u{001B}[0m";
     public function render(Map $map): void
     {
-        for ($positionY = Map::$height; $positionY > 0; $positionY--) {
+        for ($positionY = $map->getHeight(); $positionY >= Map::MIN_COORDINATE; $positionY--) {
             $line = [];
-            for ($positionX = Map::$width; $positionX > 0; $positionX--) {
-                if ($map->haveEntityOnPosition($positionX, $positionY)) {
-                    $line[] = $map->getEntity($positionX, $positionY);
+            for ($positionX = Map::MIN_COORDINATE; $positionX <= $map->getWidth(); $positionX++) {
+                $coordinates = new Coordinates($positionX, $positionY);
+                if ($map->haveEntityOnPosition($coordinates)) {
+                    $line[] = $map->getEntity($coordinates);
                 } else {
                     $emptyPart = str_repeat(self::EMPTY_SYMBOL, self::BYTES_IN_UNICODE_SYMBOL);
                     $line[] = self::ANSI_BG_COLOR .  $emptyPart . self::ANSI_RESET_COLOR;
