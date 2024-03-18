c542a6387dcea051827209a0d42992f470fc63e3
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-03-18 21:28:33 +0300

    feat: implements methods for work with coordinates

--- src/Coordinates.php
+++ src/Coordinates.php
@@ -25,4 +25,77 @@ class Coordinates
         return $this->getCoordX() . ':' . $this->getCoordY();
     }
 
+    public function getCoordX(): int
+    {
+        return $this->coordX;
+    }
+
+    public function getCoordY(): int
+    {
+        return $this->coordY;
+    }
+
+    public function getCoordinates(): Coordinates
+    {
+        return $this;
+    }
+
+    public function equals(Coordinates $coordinates): bool
+    {
+        return $this->getCoordX() === $coordinates->getCoordX() && $this->getCoordY() === $coordinates->getCoordY();
+    }
+
+    public function isCoordinateNeighbor(Coordinates $coordinates): bool
+    {
+        return $this->calcCoordinateDistance($coordinates) < 2;
+    }
+
+    public function calcCoordinateDistance(Coordinates $coordinates): float
+    {
+        return sqrt(
+            abs($this->coordX - $coordinates->getCoordX()) ** 2 +
+            abs($this->coordY - $coordinates->getCoordY()) ** 2
+        );
+    }
+
+    public function calcNeighbors(int $calcLimit = 1): array
+    {
+        $result = [];
+        $directions = 8;
+        $x = $this->coordX;
+        $y = $this->coordY;
+        for ($calcDepth = $calcLimit; $calcDepth > 0; $calcDepth--) {
+            for ($direction = 1; $direction <= $directions; $direction++) {
+                $neighborCoordinate = null;
+                switch ($direction) {
+                    case 1 :
+                        $neighborCoordinate = new self($x, $y + $calcDepth);
+                        break;
+                    case 2 :
+                        $neighborCoordinate = new self($x + $calcDepth, $y + $calcDepth);
+                        break;
+                    case 3 :
+                        $neighborCoordinate = new self($x + $calcDepth, $y);
+                        break;
+                    case 4 :
+                        $neighborCoordinate = new self($x + $calcDepth, $y - $calcDepth);
+                        break;
+                    case 5 :
+                        $neighborCoordinate = new self($x, $y - $calcDepth);
+                        break;
+                    case 6 :
+                        $neighborCoordinate = new self($x - $calcDepth, $y - $calcDepth);
+                        break;
+                    case 7 :
+                        $neighborCoordinate = new self($x - $calcDepth, $y);
+                        break;
+                    case 8 :
+                        $neighborCoordinate = new self($x - $calcDepth, $y + $calcDepth);
+                        break;
+                }
+                $result[(string)$neighborCoordinate] = $neighborCoordinate;
+            }
+        }
+        return $result;
+    }
 }
