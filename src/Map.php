6bcd692e3024e9564ca1451b08ef5a52b22c29a2
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-03-19 18:28:29 +0300

    feat: add getters for width and heigth Map class

--- src/Map.php
+++ src/Map.php
@@ -29,6 +29,16 @@ class Map
         $this->entities = [];
     }
 
+    public function getWidth(): int
+    {
+        return $this->width;
+    }
+
+    public function getHeight(): int
+    {
+        return $this->height;
+    }
+
     public function setEntity(Coordinates $coordinates, Entity $entity): void
     {
         $entity->setCoordinates($coordinates);
