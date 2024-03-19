42305ab5e95bc0fd7a83c2f7349dc4935049f21f
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-03-19 17:24:02 +0300

    refactor: remove redundant space in ConsoleMapRenderer class

--- src/ConsoleMapRenderer.php
+++ src/ConsoleMapRenderer.php
@@ -8,6 +8,7 @@ class ConsoleMapRenderer
     private const BYTES_IN_UNICODE_SYMBOL = 2;
     private const ANSI_BG_COLOR = "\u{001B}[0;100m";
     private const ANSI_RESET_COLOR = "\u{001B}[0m";
+
     public function render(Map $map): void
     {
         for ($positionY = $map->getHeight(); $positionY >= Map::MIN_COORDINATE; $positionY--) {
@@ -18,7 +19,7 @@ class ConsoleMapRenderer
                     $line[] = $map->getEntity($coordinates);
                 } else {
                     $emptyPart = str_repeat(self::EMPTY_SYMBOL, self::BYTES_IN_UNICODE_SYMBOL);
-                    $line[] = self::ANSI_BG_COLOR .  $emptyPart . self::ANSI_RESET_COLOR;
+                    $line[] = self::ANSI_BG_COLOR . $emptyPart . self::ANSI_RESET_COLOR;
                 }
             }
             $strLine = implode(' ', $line) . "\n";
