3608cc9bdc6329148f62bf869f1fbca22850099d
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-03-19 15:21:51 +0300

    refactor: change Actions class to abstract class

--- src/Actions.php
+++ src/Actions.php
@@ -2,51 +2,14 @@
 
 namespace Simulation;
 
-use Simulation\Pathfind\EnvironmentEvaluations;
+use Simulation\Coordinates;
+use Simulation\Map;
 
-class Actions
+abstract class Actions
 {
-    public function initActions(Map $map): void
-    {
-        $countEntitiesOnMapInPercent = [
-            Tree::class => 8,
-            Rock::class => 8,
-            Grass::class => 7,
-            Predator::class => 0.4,
-            Herbivore::class => 3,
-        ];
+    abstract public function action(Map $map): void;
 
-        $numberOfEntities = $this->conversionPercentEntitiesToNumbers($countEntitiesOnMapInPercent, $map);
-
-        $entityFactory = new EntityFactory(new EnvironmentEvaluations());
-
-        foreach ($numberOfEntities as $entity => $count) {
-            for ($num = $count; $num >= 0; $num--) {
-                $position = $this->generateStartRandomPosition($map);
-                $map->setEntity($position, $entityFactory->create($entity, $position));
-            }
-        }
-    }
-
-    public function turnActions(Map $map, ConsoleMapRenderer $renderer): void
-    {
-        $creatures = $map->getEntitiesByClass(Creature::class);
-        foreach ($creatures as $creature) {
-            if ($creature->getHealthPoints() <= 0) {
-                $map->removePosition($creature->getCoordinates());
-                continue;
-            }
-            for ($step = 0; $step < $creature->getSpeed(); $step++) {
-                $map->removePosition($creature->getCoordinates());
-                $coordinate = $creature->makeMovie($map);
-                $map->setEntity($coordinate, $creature);
-            }
-
-        }
-        $renderer->render($map);
-    }
-
-    public function generateStartRandomPosition(Map $map): Coordinates
+    protected function generateStartRandomPosition(Map $map): Coordinates
     {
         $x = random_int($map::MIN_COORDINATE, $map->getWidth());
         $y = random_int($map::MIN_COORDINATE, $map->getHeight());
@@ -56,18 +19,5 @@ class Actions
         }
 
         return new Coordinates($x, $y);
-    }
-
-    private function conversionPercentEntitiesToNumbers(array $countEntitiesOnMapInPercent, Map $map): array
-    {
-        $result = [];
-        $countCells = $map->getHeight() * $map->getWidth();
-        $percentDelimiter = 100;
-
-        foreach ($countEntitiesOnMapInPercent as $entity => $percent) {
-            $result[$entity] = round(($percent * $countCells) / $percentDelimiter);
-        }
-
-        return $result;
     }
 }
