0c2f671a50376fa500ceef3ecef3f0d139bef347
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-03-20 12:56:01 +0300

    feat: implement locateMoveCoordinates method in Herbivore class

--- src/Herbivore.php
+++ src/Herbivore.php
@@ -32,6 +32,30 @@ class Herbivore extends Creature
         return null;
     }
 
+    public function locateMoveCoordinates(Map $map): Coordinates
+    {
+        if ($this->getCoordinateForSimpleMove() &&
+            !$this->hasPredatorNearest($map, $this->getCoordinateForSimpleMove()) &&
+            !$this->getCoordinateForSimpleMove()->equals($this->getCoordinates())) {
+            return $this->getCoordinateForSimpleMove();
+        }
+
+        $potentialMoves = $this->generateListPositionsFromCoordinates($map);
+        $moves = $this->generateListCoordinateMoves($potentialMoves, $map);
+        $targets = new Targets($map, $moves);
+
+        $farTargets = $targets->calcDistanceBetweenTargets($this->getCoordinates())->shuffleTargets()->setFarTargetsFirst();
+
+        while($farTargets->haveTargets()) {
+            $farTarget = $farTargets->getTarget();
+            if (!$this->hasPredatorNearest($map, $farTarget)) {
+                $this->setCoordinateForSimpleMove($farTarget);
+                return $farTarget;
+            }
+        }
+        return $this->getCoordinates();
+    }
+
     }
 
     public function __toString()
