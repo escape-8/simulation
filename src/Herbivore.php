5e8274a320537f156c1384e21f0139ea036a850b
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-03-20 12:54:57 +0300

    feat: implement selectHerbivoreTarget method in Herbivore class

--- src/Herbivore.php
+++ src/Herbivore.php
@@ -11,6 +11,27 @@ class Herbivore extends Creature
         return $map->getEntitiesByClass(Grass::class);
     }
 
+    public function selectHerbivoreTarget(Targets $targets, Map $map): ?Grass
+    {
+        if (!$targets->haveTargets()) {
+            return null;
+        }
+
+        while($targets->haveTargets()) {
+            $nearestTarget = $targets->getTarget();
+
+            if ($this->getCoordinates()->isCoordinateNeighbor($nearestTarget->getCoordinates())) {
+                return $nearestTarget;
+            }
+
+            if (!$this->hasPredatorNearest($map, $nearestTarget->getCoordinates())) {
+                return $nearestTarget;
+            }
+        }
+
+        return null;
+    }
+
     }
 
     public function __toString()
